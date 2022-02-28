d79b2d333f4286cd407bf6fac7e84206e7daf553
Uldis Kirsis <uldis@sit.lv>
2022-02-28 15:32:54 +0200

    New portion of data

--- transform.php
+++ transform.php
@@ -6,14 +6,19 @@ $reader = new Reader();
 $json = [];
 while ($row = $reader->read())
 {
+    if (empty($row['institution']))
+    {
+        user_error("No institution name");
+        continue;
+    }
+    
     if (!isset($row['lat']))
     {
         user_error("No coordinates for " . $row['institution']);
-    } else 
-    {
-        $json[] = $row;
-    }
+        continue;
+    } 
     
+    $json[] = $row;
 }
 
 $reader->_close();
