f12094a6c3a2d0f778eb200e6453580539016e87
Uldis Kirsis <uldis@sit.lv>
2022-03-02 16:11:08 +0200

    Generated final pages

--- views/help.php
+++ views/help.php
@@ -1,52 +1,13 @@
-<!doctype html>
-<html lang="lv">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="#ScienceForUkraine is a voluntary initiative whose mission is to support students and researchers from Ukraine directly affected by the Russia’s invasion." />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="Last-Modified" content="Wed, 02 Mar 2022 13:24:07 GMT" />
-    
-    <title>Science for Ukraine - Help</title>
-    <link href="assets/leaflet/leaflet.css" rel="stylesheet">
-    <link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/local_css/default_page.css" rel="stylesheet">
-        
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BVWXGWBK9R"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-BVWXGWBK9R');
-    </script>
-  </head>
+<?php
+/**
+ * Help page view
+ *
+ * @var Page $page
+ */
 
-  <body>
-    <div class="row info">
-      <div class="col order-lg-2">
-        <a href="index.html">
-          <img src="assets/sfu-banner.png" alt="Science For Ukraine " class="logo" />
-        </a>
-      </div>
-      <div class="col nav order-lg-1">
-        <p>Navigation</p>
-        <ul>          
-          <li><a href="help.html">How you can help</a></li>
-          <li><a href="team.html">Team/Contact</a></li>
-          <li><a href="https://twitter.com/Sci_for_Ukraine" target="_blank"><svg
-                class="bi" width="16" height="16"
-                fill="currentColor">
-                <use xlink:href="assets/bootstrap-icons.svg#twitter" /></svg>
-                Follow us on Twitter
-        </a></li>
-        </ul>
-      </div>
-      <div class="col order-lg-3"></div>
-    </div>
-
-    <div class="container info">
+$page->title = "Science for Ukraine - Help";
+?>
+<div class="container info">
     <h2>How you can help</h2>
     
     <h3>Institutions</h3>
@@ -97,13 +58,3 @@
 <div class="container bottom">
   <a href="/">Back</a>
 </div>
-
-    <script src="assets/leaflet/leaflet.js"></script>
-    <script src="assets/load_map.js"></script>
-    <script>L.Icon.Default.imagePath = 'assets/leaflet/images/';</script>  
-    <script src="data.js?v=1646227447"></script>
-    <script src="draw.js?v=3"></script>
-</body>
-
-
-</html>
