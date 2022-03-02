d031733ec8b9faf8bf86ee3c87e8f9d00cf846c4
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2022-03-02 16:17:13 +0100

    Update statement

--- views/about.php
+++ views/about.php
@@ -11,8 +11,8 @@ $page->title = "Science for Ukraine - About";
 ?>
 <div class="container info">
     <h3>Mission Statement</h3>
-    <p>#ScienceForUkraine is a group of volunteering scholars from academic institutions in Europe and all over the world.</p>
-    <p>Our mission is to collect and disseminate information about support opportunities at academic institutions for scholars fleeing Ukraine. Such forms of support prevent the break-up of scholarly careers and enable researchers to sustain themselves and their families while abroad by doing what they know and love to do. We believe that scientific cooperation helps in maintaining Ukraine’s presence in the European Research Area and international scholarly community.</p>
+    <p>#ScienceForUkraine is a community group of volunteer students and research scientists from academic institutions in Europe and around the world.</p>
+    <p>Our mission is to collect and disseminate information about support opportunities at the university, national, and international level for Ukrainian students and researchers directly affected by the recent Russian invasion of Ukraine. These support opportunities are directed towards developing and promoting the scientific careers and research of Ukrainians and offering financial support to researchers and their families during these troubling times. We believe that unity and collaboration can help maintain Ukraine’s long-standing excellence in the European and international research communities.</p>
     <p>We are grateful for support from <a href="https://nep4dissent.eu/" target="_blank">NEP4DISSENT</a>, <a href="http://lulfmi.lv/en/news">ILFA at University of Latvia's Institute</a> and the <a href="https://amu.pan.pl/en/" target="_blank">Polish Young Academy - Polish Academy of Sciences</a>
 </div>
 
