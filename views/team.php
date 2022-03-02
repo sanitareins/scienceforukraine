3b68c451f7a939de8720676420de24091167f083
Uldis Kirsis <uldis@sit.lv>
2022-03-02 12:58:34 +0200

    team template

---
+++ views/team.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Team page
+ *
+ * @var Page $page
+ */
+
+$page->title = "Science for Ukraine - Team";
+?>
+<div class="container info">
+
+    <h3>Coordination</h3>
+    <p>
+        This page is coordinated by <b>Sanita Reinsone</b> <a
+            href="mailto:sanita.reinsone@lulfmi.lv"> <svg
+                class="bi" width="16" height="16"
+                fill="currentColor">
+                <use xlink:href="assets/bootstrap-icons.svg#at" /></svg></a>
+        <a href="https://twitter.com/sanitare" target="_blank"><svg
+                class="bi" width="16" height="16"
+                fill="currentColor">
+                <use
+                    xlink:href="assets/bootstrap-icons.svg#twitter" /></svg>
+        </a>
+        
+        and her team at <a href="http://lulfmi.lv/en/news">University of Latvia's Institute of Literature, Folklore and Art of the University of Latvia (ILFA)</a>:
+        <ul>
+            <li>Elīna Gailīte</li>
+            <li>Justīne Jaudzema</li>
+            <li>Ilze Ļaksa-Timinska</li>
+            <li>Antra Upeniece</li>
+        </ul>
+    </p>
+
+    <h3>Country coordinators</h3>
+    <p>The task of country coordinators is to be in <b>contact with other platforms</b> to coordinate efforts, to follow what's happening in their country in regard to support activities and to <b>add national data</b> (in collected form - if such is provided by any organization).</p>
+
+    <p>If you are willing to help, get <a href="mailto:sanita.reinsone@lulfmi.lv" target="_blank">in touch</a>!</p>
+    <b>Canada</b>
+    <ul>
+      <li>Brokoslaw Laschowski</li>
+      <li>E-mail: <a href="mailto:brock.laschowski@mail.utoronto.ca" target="_blank">Brock.Laschowski@mail.utoronto.ca</a></li>
+      <li>Twitter: <a href="https://twitter.com/DrLaschowski" target="_blank">@DrLaschowski</a></li>
+    </ul>
+    <b>Finland</b>
+    <ul>
+      <li>Annina Lattu</li>
+      <li>E-mail: <a href="mailto:annina.lattu@tuni.fi" target="_blank">Annina.Lattu@tuni.fi</a></li>
+      <li>Twitter: <a href="https://twitter.com/anninalattu" target="_blank">@anninalattu</a></li>
+    </ul>
+    <b>Moldova</b>
+    <ul>
+      <li>Maria Caraman</li>
+      <li>E-mail: <a href="mailto:caraman.c.maria@gmail.com" target="_blank">Caraman.C.Maria@gmail.com</a></li>
+    </ul>
+    <b>Germany</b>
+    <ul>
+      <li>Michael Rose</li>
+      <li>E-mail: <a href="mailto:Michael.Rose@ip.mpg.de" target="_blank">Michael.Rose@ip.mpg.de</a></li>
+      <li>Twitter: <a href="https://twitter.com/Michael_E_Rose" target="_blank">@Michael_E_Rose</a></li>
+    </ul>
+    <b>Latvia</b>
+    <ul>
+      <li>Ieva Tihovska</li>
+      <li>E-mail: <a href="mailto:ieva.tihovska@lulfmi.lv" target="_blank">Ieva.Tihovska@lulfmi.lv</a></li>
+      <li>Twitter: <a href="https://twitter.com/itaadiete" target="_blank">@itaadiete</a></li>
+    </ul>
+    <b>Netherlands</b>
+    <ul>
+      <li>Oleksandra Ivashchenko  </li>
+      <li>E-mail: <a href="mailto:o.ivashchenko@lumc.nl" target="_blank">O.Ivashchenko@lumc.nl</a></li>
+      <li>Twitter: <a href="https://twitter.com/OleksandraIvas3" target="_blank">@OleksandraIvas3</a></li>
+    </ul>
+    <b>Slovenia</b>
+    <ul>
+      <li>Maryna Shevtsova</li>
+      <li>E-mail: <a href="mailto:Shevtsova.Maryna@gmail.com" target="_blank">Shevtsova.Maryna@gmail.com</a></li>
+    </ul>
+    <b>Switzerland</b>
+    <ul>
+      <li>Johannes Knörzer</li>
+      <li>E-mail: <a href="mailto:johannes.knoerzer@eth-its.ethz.ch" target="_blank">Johannes.Knoerzer@eth-its.ethz.ch</a></li>
+      <li>Twitter: <a href="https://twitter.com/KnorzerJohannes" target="_blank">@KnorzerJohannes</a></li>
+    </ul>
+    <b>Poland</b>
+    <ul>
+      <li>Anna Kłos</li>
+      <li>E-mail: <a href="mailto:anna.klos@wat.edu.pl" target="_blank">Anna.Klos@wat.edu.pl</a></li>
+      <li>Twitter: <a href="https://twitter.com/ania_klos" target="_blank">@ania_klos</a></li>
+    </ul>
+    <b>Romania</b>
+    <ul>
+      <li>Cristina Rujan</li>
+      <li>E-mail: <a href="mailto:cristina.rujan@ip.mpg.de" target="_blank">Cristina.Rujan@ip.mpg.de</a></li>
+      <li>Twitter: <a href="https://twitter.com/Cris_Rjn" target="_blank">@Cris_Rjn</a></li>
+    </ul>
+
+    <h3>Website</h3>
+    <ul>
+      <li>Uldis Ķirsis<a href="https://twitter.com/UKirsis" target="_blank"><svg class="bi" width="16" height="16" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#twitter"/></svg></a> from <a href="http://lulfmi.lv/en/news" target="_blank">University of Latvia, ILFA</a></li>
+      <li>Michael Rose<a href="https://twitter.com/Michael_E_Rose" target="_blank"><svg class="bi" width="16" height="16" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#twitter"/></svg></a> from <a href="https://www.ip.mpg.de/en/" target="_blank">Max Planck Institute for Innovation and Competition</a></li>
+    </ul>
+    hosted at <a href="https://github.com/zucha/scienceforukraine" target="_blank">GitHub</a>
+
+    <p>We are grateful for support from <a href="https://nep4dissent.eu/" target="_blank">NEP4DISSENT</a>, <a href="http://lulfmi.lv/en/news">ILFA at University of Latvia's Institute</a> and the <a href="https://amu.pan.pl/en/" target="_blank">Polish Young Academy - Polish Academy of Sciences</a>
+</div>
+
+<div class="container bottom">
+  <a href="/">Back</a>
+</div>
